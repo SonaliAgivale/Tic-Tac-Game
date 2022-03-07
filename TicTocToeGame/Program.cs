cb850a4af6524fa8066dae7ec8d78a0778933447
sonali4598 <sonaliagivale11093@gmail.com>
2022-03-07 09:44:18 +0530

    [sonali Agivale] created Tic Toc Toe Game

---
+++ TicTocToeGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTocToeGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Tic Toc Toe ******");
+            TicTocToeGame.createBoard();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TicTocToeGame/TicTocToeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTocToeGame
+{
+    public class TicTocToeGame
+    {
+        public static void createBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 1; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
